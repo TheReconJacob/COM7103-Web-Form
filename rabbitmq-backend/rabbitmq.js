5cd5e51faa55c5b98941b3bfaa630df12c71f9f7
thereconjacob <jvpokeyjake@gmail.com>
2024-08-16 13:34:40 +0100

    restructure and set up backend

---
+++ rabbitmq-backend/rabbitmq.js
@@ -0,0 +1,17 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, connection) => {
+  if (err) throw err;
+
+  connection.createChannel((err, channel) => {
+    if (err) throw err;
+
+    const queue = 'my-queue';
+    channel.assertQueue(queue, { durable: false });
+
+    // Publish a message
+    channel.sendToQueue(queue, Buffer.from('Hello from RabbitMQ!'));
+
+    console.log('Message sent to RabbitMQ');
+  });
+});
